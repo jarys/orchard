ed150fe33dcac299222f228e5d7b781f2c4f6eb6
Tomas Krnak <tomas@krnak.cz>
2021-10-14 15:05:41 +0200

    std flag for all modules

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 //! implicitly mean it is an Orchard payment address (as opposed to e.g. a Sapling payment
 //! address, which is also shielded).
 
+#![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // Temporary until we have more of the crate implemented.
 #![allow(dead_code)]
@@ -16,25 +17,43 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 
+#[cfg(feature = "std")] // TODO: make it no_std
 mod address;
+#[cfg(feature = "std")]
 pub mod builder;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod bundle;
+#[cfg(feature = "std")]
 pub mod circuit;
+#[cfg(feature = "std")] // TODO: make it no_std
 mod constants;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod keys;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod note;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod note_encryption;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod primitives;
+#[cfg(feature = "std")] // TODO: make it no_std
 mod spec;
+#[cfg(feature = "std")]
 pub mod tree;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod value;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub mod zip32;
 
-#[cfg(test)]
+#[cfg(all(test, feature = "std"))]
 mod test_vectors;
 
+#[cfg(feature = "std")] // TODO: make it no_std
 pub use address::Address;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub use bundle::Bundle;
+#[cfg(feature = "std")]
 pub use circuit::Proof;
+#[cfg(feature = "std")] // TODO: make it no_std
 pub use note::Note;
+#[cfg(feature = "std")]
 pub use tree::Anchor;

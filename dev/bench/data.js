e4a31077e61b1c0a09fca2e57329770ebd4155a5
github-action-benchmark <github@users.noreply.github.com>
2022-05-05 16:20:59 +0000

    add Orchard Benchmarks (cargo) benchmark result for 93ad4a6952e41465ea4e046d0a54deef54d8731d

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1651767659766,
+  "repoUrl": "https://github.com/jarys/orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "ewillbefull@gmail.com",
+            "name": "ebfull",
+            "username": "ebfull"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "93ad4a6952e41465ea4e046d0a54deef54d8731d",
+          "message": "Merge pull request #318 from zcash/str4d/circuit-review\n\nChanges from circuit review",
+          "timestamp": "2022-05-05T09:24:12-06:00",
+          "tree_id": "708d87a26bba0ec133046557b4f12258f212b8f2",
+          "url": "https://github.com/jarys/orchard/commit/93ad4a6952e41465ea4e046d0a54deef54d8731d"
+        },
+        "date": 1651767658652,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 5350553402,
+            "range": "± 88336121",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 5466565808,
+            "range": "± 155458958",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 7817229052,
+            "range": "± 237960372",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 10035757431,
+            "range": "± 138018230",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/1",
+            "value": 42873672,
+            "range": "± 2586920",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/2",
+            "value": 40945192,
+            "range": "± 2030421",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/3",
+            "value": 49051455,
+            "range": "± 2969965",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/4",
+            "value": 50924140,
+            "range": "± 3773255",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
